2f128f3109175272fedb91424cabccdcd0e1e2d9
Prasanna <prasanna1157@gmail.com>
2020-01-11 14:37:05 +1100

    Remove print statement.

--- src/main/java/recipefinder/recipefinder/RecipeFinderApplication.java
+++ src/main/java/recipefinder/recipefinder/RecipeFinderApplication.java
@@ -2,16 +2,11 @@ package recipefinder.recipefinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class RecipeFinderApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(RecipeFinderApplication.class, args);
-		for (String name : applicationContext.getBeanDefinitionNames()) {
-			System.out.println(name);
+		SpringApplication.run(RecipeFinderApplication.class, args);
 		}
 	}
-
-}
